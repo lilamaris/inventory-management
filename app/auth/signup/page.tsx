b43860c76d98acd1af438bd99cb8f8579f426075
lilamaris <lilamaris.dev@gmail.com>
2025-06-08 16:31:11 +0900

    feat: add sign-in and sign-up pages, implement authentication forms, and integrate Radix UI components for improved UI

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,42 @@
+import { SignUpForm } from '@/components/auth/signup-form'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { GalleryVerticalEnd } from 'lucide-react'
+import Link from 'next/link'
+
+export default function SignUpPage() {
+    return (
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <div className="flex w-full max-w-sm flex-col gap-6">
+                <Link href="/" className="flex items-center gap-2 self-center font-medium">
+                    <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                        <GalleryVerticalEnd className="size-4" />
+                    </div>
+                    Inventory Inc.
+                </Link>
+
+                <div className="flex flex-col gap-6">
+                    <Card>
+                        <CardHeader className="text-center">
+                            <CardTitle className="text-xl">Welcome</CardTitle>
+                            <CardDescription>Sign up with your</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                                <span className="bg-card text-muted-foreground relative z-10 px-2">
+                                    Or continue with
+                                </span>
+                            </div>
+                            <SignUpForm />
+                            <div className="text-center text-sm">
+                                Already have an account?{' '}
+                                <Link href="/auth/signin" className="text-sm underline-offset-4 hover:underline">
+                                    Sign in
+                                </Link>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    )
+}
