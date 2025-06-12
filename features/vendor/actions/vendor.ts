605b986d095dff3330367a34aa2e9befe26e2bfa
lilamaris <lilamaris.dev@gmail.com>
2025-06-12 17:27:11 +0900

    feat: implement update vendor functionality with form validation and error handling for improved vendor management

--- features/vendor/actions/vendor.ts
+++ features/vendor/actions/vendor.ts
@@ -1,7 +1,12 @@
 'use server'
 
 import { prisma } from '@/lib/prisma'
-import { createVendorSchema, type CreateVendorState } from '@/features/vendor/types/vendor'
+import {
+    createVendorSchema,
+    updateVendorSchema,
+    UpdateVendorState,
+    type CreateVendorState,
+} from '@/features/vendor/types/vendor'
 import { revalidatePath } from 'next/cache'
 
 export const createVendorAction = async (state: CreateVendorState, formData: FormData) => {
@@ -35,6 +40,43 @@ export const createVendorAction = async (state: CreateVendorState, formData: For
             formData,
             success: false,
             message: 'An error occurred while creating the vendor',
+        }
+    }
+}
+
+export const updateVendorAction = async (state: UpdateVendorState, formData: FormData) => {
+    const formDataEntries = Object.fromEntries(formData)
+    console.log(formDataEntries)
+
+    const validatedFields = updateVendorSchema.safeParse(formDataEntries)
+
+    if (!validatedFields.success)
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            formData,
+            success: false,
+            message: 'Invalid form data',
+        }
+
+    const { id, name, description } = validatedFields.data
+
+    try {
+        const vendor = await prisma.vendor.update({
+            where: { id },
+            data: { name, description },
+        })
+        revalidatePath('/')
+        return {
+            formData,
+            success: true,
+            message: 'Vendor updated successfully',
+        }
+    } catch (error) {
+        console.error('Error updating vendor: ', error)
+        return {
+            formData,
+            success: false,
+            message: 'An error occurred while updating the vendor',
         }
     }
 }
