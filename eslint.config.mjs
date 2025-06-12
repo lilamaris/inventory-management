4b9403cde87c23c45454c535a0a0938077148651
lilamaris <lilamaris.dev@gmail.com>
2025-06-12 18:32:03 +0900

    refactor: simplify eslint configuration by removing redundant import restrictions for improved clarity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,62 +9,6 @@ const compat = new FlatCompat({
     baseDirectory: __dirname,
 })
 
-const eslintConfig = [
-    ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-    {
-        'import/no-restricted-paths': [
-            'error',
-            {
-                zones: [
-                    // disable cross-feature imports:
-                    // eg. src/features/auth should not import from src/features/product
-                    {
-                        target: './features/auth',
-                        from: './features',
-                        message: 'Cross-feature imports are not allowed',
-                        except: ['./auth'],
-                    },
-                    {
-                        target: './features/product',
-                        from: './features',
-                        message: 'Cross-feature imports are not allowed',
-                        except: ['./product'],
-                    },
-                    {
-                        target: './features/user',
-                        from: './features',
-                        message: 'Cross-feature imports are not allowed',
-                        except: ['./user'],
-                    },
-                    {
-                        target: './features/order',
-                        from: './features',
-                        message: 'Cross-feature imports are not allowed',
-                        except: ['./order'],
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        'import/no-restricted-paths': [
-            'error',
-            {
-                zones: [
-                    // enforce unidirectional codebase:
-                    // eg. src/app can import from src/features but not the other way around
-                    {
-                        target: './features',
-                        from: './app',
-                    },
-                    {
-                        target: ['./components', './hooks', './lib', './utils', './types'],
-                        from: ['./features', './app'],
-                    },
-                ],
-            },
-        ],
-    },
-]
+const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier')]
 
 export default eslintConfig
