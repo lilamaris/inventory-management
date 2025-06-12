02fa618bfeaa9cb3a8826b852d2164944e4ebc04
lilamaris <lilamaris.dev@gmail.com>
2025-06-12 18:08:16 +0900

    style: adjust skeleton component heights in vendor table skeleton for improved visual consistency

--- features/vendor/components/skeleton/vendor-table-skeleton.tsx
+++ features/vendor/components/skeleton/vendor-table-skeleton.tsx
@@ -6,11 +6,11 @@ export function VendorTableSkeleton() {
     return (
         <div className="flex flex-col gap-2">
             <div className="flex items-center gap-2">
-                <Skeleton className="h-10 w-24" />
-                <Skeleton className="h-10 w-24" />
-                <Skeleton className="h-10 w-24" />
-                <Skeleton className="ml-auto h-10 w-16" />
-                <Skeleton className="h-10 w-32" />
+                <Skeleton className="h-8 w-24" />
+                <Skeleton className="h-8 w-24" />
+                <Skeleton className="h-8 w-24" />
+                <Skeleton className="ml-auto h-8 w-16" />
+                <Skeleton className="h-8 w-32" />
             </div>
             <TableSkeleton columns={vendorColumns.length} />
         </div>
