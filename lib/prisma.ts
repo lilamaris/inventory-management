7e06346067fc5c76b60a95b53e7de1e41c6cecce
lilamaris <lilamaris.dev@gmail.com>
2025-06-18 22:12:26 +0900

    prisma: enhance Prisma client with query logging and event handling

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,35 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        log: [
+            {
+                emit: 'event',
+                level: 'query',
+            },
+            {
+                emit: 'event',
+                level: 'error',
+            },
+            {
+                emit: 'event',
+                level: 'info',
+            },
+            {
+                emit: 'event',
+                level: 'warn',
+            },
+        ],
+    }).$extends(withAccelerate())
+
+// 쿼리 로깅 이벤트 리스너
+prisma.$on('query', (e: any) => {
+    console.log('Query: ' + e.query)
+    console.log('Params: ' + e.params)
+    console.log('Duration: ' + e.duration + 'ms')
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
