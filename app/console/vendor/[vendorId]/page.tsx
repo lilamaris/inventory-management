76bb600562105d27ce004d426ac3c45c8d525da1
lilamaris <lilamaris.dev@gmail.com>
2025-06-22 01:32:37 +0900

    feat: enhance VendorPage to display vendor items in a table format with detailed information

--- app/console/vendor/[vendorId]/page.tsx
+++ app/console/vendor/[vendorId]/page.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { getVendorById, getVendorWithPurchaseOrdersById } from '@/features/vendor/service/vendor'
 import { getCurrentSession } from '@/lib/server/session'
 import { redirect } from 'next/navigation'
@@ -18,7 +19,7 @@ export default async function VendorPage({ params }: { params: PathParams }) {
     }
 
     return (
-        <div>
+        <div className="flex flex-col gap-6">
             <h1>{vendor.name}</h1>
             <p>{vendor.description}</p>
             <p>Managers: {vendor.managers.map((manager) => manager.user.name).join(', ')}</p>
@@ -28,11 +29,29 @@ export default async function VendorPage({ params }: { params: PathParams }) {
                     <CardTitle>Items</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <div className="flex flex-col gap-2">
-                        {vendor.items.map((item) => (
-                            <div key={item.id}>{item.name}</div>
-                        ))}
-                    </div>
+                    <Table>
+                        <TableCaption>A list of items provided by the vendor.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Name</TableHead>
+                                <TableHead>Description</TableHead>
+                                <TableHead>Price</TableHead>
+                                <TableHead>Quantity</TableHead>
+                                <TableHead>Total</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {vendor.items.map((item) => (
+                                <TableRow key={item.id}>
+                                    <TableCell>{item.name}</TableCell>
+                                    <TableCell>{item.description}</TableCell>
+                                    <TableCell>{item.price}</TableCell>
+                                    <TableCell>{item.quantity}</TableCell>
+                                    <TableCell>{item.price * item.quantity}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
                 </CardContent>
             </Card>
         </div>
