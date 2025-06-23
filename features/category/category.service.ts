ef8d3ca000bbae148fa5361efdab9ec93a8bf53e
lilamaris <lilamaris.dev@gmail.com>
2025-06-23 13:35:04 +0900

    feat: implement category DTO and service for CRUD operations, including create, update, and delete functionalities

---
+++ features/category/category.service.ts
@@ -0,0 +1,26 @@
+import { Category } from '@/features/category/category.dto'
+import prisma from '@/lib/prisma'
+
+export async function createCategory(category: Category): Promise<Category> {
+    const { name, vendorId } = category
+    const newCategory = await prisma.category.create({
+        data: { name, vendorId },
+    })
+    return newCategory
+}
+
+export async function updateCategory(category: Category): Promise<Category> {
+    const { id, name } = category
+    const updatedCategory = await prisma.category.update({
+        where: { id },
+        data: { name },
+    })
+    return updatedCategory
+}
+
+export async function deleteCategory(id: string): Promise<Category> {
+    const deletedCategory = await prisma.category.delete({
+        where: { id },
+    })
+    return deletedCategory
+}
